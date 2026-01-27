982e15bb540004d33b8f7c01175c6b66df0b921d
jeongseok <ljs0429777@gmail.com>
2026-01-27 09:29:55 +0900

    chore: style-dictionary 의존성 추가

---
+++ style-dictionary.config.js
@@ -0,0 +1,25 @@
+import { register } from '@tokens-studio/sd-transforms';
+import StyleDictionary from 'style-dictionary';
+
+register(StyleDictionary);
+
+const sd = new StyleDictionary({
+  source: ['./token/default-token.json'], // Tokens Studio 에서 추출한 토큰
+  preprocessors: ['tokens-studio'],
+  platforms: {
+    css: {
+      transformGroup: 'tokens-studio', // <-- apply the tokens-studio transformGroup to apply all transforms
+      transforms: ['name/kebab'], // 만들어질 token 이름 형태, 기본값 camel
+      buildPath: '', // 생성될 파일 경로
+      files: [
+        {
+          destination: 'tokens.json', // 반환될 토큰 파일 이름
+          format: 'json',
+        },
+      ],
+    },
+  },
+});
+
+await sd.cleanAllPlatforms();
+await sd.buildAllPlatforms();
