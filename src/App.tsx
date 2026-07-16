a17595d52d0ccd2858d55622ef7de94a245e46ff
jeongseok <ljs0429777@gmail.com>
2026-07-16 11:56:13 +0900

    feat: 방명록 페이지와 /guestbook 라우트 연결

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom'
 import { Analytics } from '@vercel/analytics/react'
 import HomePage from './pages/HomePage'
 import ProjectDetailPage from './pages/ProjectDetailPage'
+import GuestbookPage from './guestbook/GuestbookPage'
 import ScrollManager from './components/ScrollManager'
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/projects/:id" element={<ProjectDetailPage />} />
+        <Route path="/guestbook" element={<GuestbookPage />} />
       </Routes>
       <Analytics />
     </>
